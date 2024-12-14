6f522e7eabdb0b6b56a6b4dc723c9a902492a2fb
Jerry Zou <zhihui.zou@duke.edu>
2024-12-14 18:10:58 -0500

    updated interface buttons and selection function.

--- ProgramCode/ToolInterface.py
+++ ProgramCode/ToolInterface.py
@@ -6,6 +6,8 @@ class XMLToolInterface:
     def __init__(self, XMLSourceDirectory):
         self.XMLSourceDirectory = XMLSourceDirectory
         self.interfaceResultLabel = None
+        self.singleFileProcessCounter = 0
+        self.folderFileProcessCounter = 0
 
     def selectionInterface(self):
         rootInterface = tk.Tk()
@@ -13,25 +15,75 @@ class XMLToolInterface:
         rootInterface.geometry("600x600")
 
         fileButton = tk.Button(rootInterface, text = "Process single XML file", command = self.processSingleFile, width = 25, height = 8)
-        fileButton.pack(pady=10)
-        self.interfaceResultLabel = tk.Label(rootInterface, text="No file or folder selected yet.", wraplength=500)
-        self.interfaceResultLabel.pack(pady=20)
+        fileButton.pack(pady = 10)
+
+        directoryButton = tk.Button(rootInterface, text = "Process directory that contains XML files", command = self.processDirectory, width = 25, height = 8)
+        directoryButton.pack(pady = 10)
+
+        exitButton = tk.Button(rootInterface, text = "End Program.", command = rootInterface.destroy, width = 20, height = 6)
+        exitButton.pack(pady = 10)
+
+        self.interfaceResultLabel = tk.Label(rootInterface, text = "No file or folder selected yet.", wraplength = 500)
+        self.interfaceResultLabel.pack(pady = 20)
+        
         rootInterface.mainloop()
 
     def processSingleFile(self):
-        singleFilePath = filedialog.askopenfilename(title="Select a File")
-        if singleFilePath:
-            self.interfaceResultLabel.config(text=f"Selected File: {singleFilePath}")
-            
+        singleFilePath = filedialog.askopenfilename(title = "Select an XML File", filetypes = [("XML Files", "*.xml")])
+        self.interfaceResultLabel.update()
 
-    # def processSingleFolder(self):
+        if not singleFilePath:  # User canceled the dialog
+            self.interfaceResultLabel.config(text = "File selection canceled. Please try again.")
+            self.interfaceResultLabel.update()
+            return
 
-    def operations(self):
+        self.interfaceResultLabel.config(text = "Please select a directory to store the output file.")
+        self.interfaceResultLabel.update()
+        self.interfaceResultLabel.after(1000)
+
+        outputDirectory = filedialog.askdirectory(title = "Select an Output Directory")
+        if not outputDirectory:
+            self.interfaceResultLabel.config(text = "Output directory selection canceled. Please try again.")
+            self.interfaceResultLabel.update()
+            return
+        
+        self.singleFileProcessCounter += 1
+        outputSingleFileName = f"output-{self.singleFileProcessCounter}.txt"
+
+        outputFilePath = f"{outputDirectory}/{outputSingleFileName}"
+
+        self.createSingleFileOutput(singleFilePath, outputFilePath)
+        
+    def processDirectory(self):
+        pass
+
+    def createSingleFileOutput(self, XMLPath, outputSingleFileName):
+        if not XMLPath:
+            self.interfaceResultLabel.config(text="No file selected. Please select an XML file.")
+            self.interfaceResultLabel.update()  # Ensure the message updates immediately
+
+        '''
+        TODO: customize which tags to export
+        '''
         testTags = ['TITLESTMT', 'TITLE', 'AUTHOR', 'EXTENT', 'PUBLICATIONSTMT']
-        XMLExtractionMachine = XMLContentExtraction(self.XMLSourceDirectory, testTags)
+        XMLExtractionMachine = XMLContentExtraction(XMLPath, testTags, outputSingleFileName)
+
         XMLExtractionMachine.traverseXML()
+        self.interfaceResultLabel.config(text = "Single XML processing completed. Please select either 'Process single XML file' or 'Process folder' to perform another export or click 'End Program' to exit.")
+        self.interfaceResultLabel.update()
+
 
 if __name__ == "__main__":
     XMLSourceDirectory = "/Users/Jerry/Desktop/DH proj-reading/XMLInterface/XMLTraversalTest/A16864.P4.xml"
     XMLInterfaceToolMachine = XMLToolInterface(XMLSourceDirectory)
     XMLInterfaceToolMachine.selectionInterface()
+
+
+
+
+    '''
+    TODO: create try except so that if a folder exists, ask user to create a new folder.
+    try:
+        XMLExtractionMachine.traverseXML()
+    except FileExistsError:
+    '''
--- ProgramCode/XMLContentExtractionObject.py
+++ ProgramCode/XMLContentExtractionObject.py
@@ -1,9 +1,10 @@
 import xml.etree.ElementTree as ET, os
 
 class XMLContentExtraction:
-    def __init__ (self, XMLSourcePath, XMLCustomizedTagsList):
+    def __init__ (self, XMLSourcePath, XMLCustomizedTagsList, outputName):
         self.XMLSourcePath = XMLSourcePath
         self.XMLCustomizedTagsList = XMLCustomizedTagsList
+        self.outputName = outputName
 
     def traverseXML(self):
         tree = ET.parse(self.XMLSourcePath)
@@ -20,11 +21,11 @@ class XMLContentExtraction:
         print(allTags)
         filteredAllContent = [item for item in allContent if item is not None]
         print("".join(filteredAllContent))
-        directory = "XMLInterface/testFolder"
+        # directory = "XMLInterface/supertestWeird"
         # os.makedirs(directory)
-        # newTestFilePath = directory + "/" + "test.txt"
-        # with open(newTestFilePath, "w") as testFile:
-        #     testFile.write("".join(filteredAllContent))
+        newTestFilePath = self.outputName
+        with open(newTestFilePath, "w") as testFile:
+            testFile.write("".join(filteredAllContent))
 
 if __name__ == "__main__":
     XMLSourcePath = "/Users/Jerry/Desktop/DH proj-reading/XMLInterface/XMLTraversalTest/A16864.P4.xml"
